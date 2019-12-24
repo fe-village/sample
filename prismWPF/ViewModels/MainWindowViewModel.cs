9e5a286d68bd21e84b7885a48f0fcf690c5a5f92
fe-village <murakami.t@ustec.co.jp>
2019-12-24 10:24:16 +0900

    Initial Commtit

---
+++ prismWPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Prism.Mvvm;
+
+namespace prismWPF.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "PrismWPF";
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+
+        }
+    }
+}
